f916ac367820cb19ff4c349a195190d383652a64
dadan nurmaulana <dadannurmaulana@gmail.com>
2023-03-18 07:28:53 +0700

    Edit Sosial Media

--- resources/js/Layouts/Sidebar.jsx
+++ resources/js/Layouts/Sidebar.jsx
@@ -64,6 +64,64 @@ const Sidebar = () => {
                         ))}
                     </ul>
                 </div>
+                <div className="mb-3 p-3 bg-body rounded shadow-sm">
+                    <h3 className="border-b pb-2 border-dashed  m-0 fw-bold">
+                        Ikuti Kami
+                    </h3>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://www.facebook.com/websitelokersubang"
+                                target="_blank"
+                                className="nav-link text-muted text-decoration-none"
+                            >
+                                <i className="fa-brands text-2xl fa-facebook pr-3"></i>{" "}
+                                Facebook
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://www.instagram.com/lokersubang_com/"
+                                target="_blank"
+                                className="nav-link text-muted text-decoration-none"
+                            >
+                                <i className="fa-brands text-2xl fa-instagram pr-3"></i>{" "}
+                                Instagram
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://t.me/loker_subang"
+                                target="_blank"
+                                className="nav-link text-muted text-decoration-none"
+                            >
+                                <i className="fa-brands text-2xl fa-telegram pr-3"></i>{" "}
+                                Telegram
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://twitter.com/loker_subang"
+                                target="_blank"
+                                className="nav-link text-muted text-decoration-none"
+                            >
+                                <i className="fa-brands text-2xl fa-twitter pr-3"></i>{" "}
+                                Twitter
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                href="https://www.linkedin.com/in/loker-subang/"
+                                target="_blank"
+                                className="nav-link text-muted text-decoration-none"
+                            >
+                                <i className="fa-brands text-2xl fa-linkedin pr-3"></i>{" "}
+                                Linkedin
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
