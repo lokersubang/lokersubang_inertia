9b3fa0d4bc38a643d2165d872dbb8dd64859386d
Dadan28 <dadannurmaulana@gmail.com>
2023-03-20 14:30:18 +0700

    edit ads

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -58,39 +58,6 @@
     <link rel='dns-prefetch' href='//code.jquery.com' />
     <link rel='dns-prefetch' href='//stackpath.bootstrapcdn.com' />
 
-    <script src="/js/propellerads.js"></script>
-
-    <script type='text/javascript'>
-        $(document).ready(function() {
-                    $( & #39;img# closed & #39;).click(function(){$(&# 39; #btm_banner & #39;).hide(90);});}); 
-    </script>
-
-    <!--start: floating ads-->
-    <div id='floatads'
-        style='width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999'>
-        <div><a id='close-floatads'
-                onclick='document.getElementById(&apos;floatads&apos;).style.display = &apos;none&apos;;'
-                style='cursor:pointer;'><img alt='close'
-                    src='https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif'
-                    title='close button' /></a></div>
-        <div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
-
-            <!--Script iklan-->
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3755518164222853"
-                crossorigin="anonymous"></script>
-            <!-- ads resp -->
-            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3755518164222853"
-                data-ad-slot="7521179607" data-ad-format="auto" data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-            <!--Akhir script iklan-->
-
-        </div>
-    </div>
-    <!--end: floating ads-->
-
-
     <!-- Scripts -->
     @routes
     @viteReactRefresh
