535e67b482fbc4131974b372b237be001712afba
dadan nurmaulana <dadannurmaulana@gmail.com>
2023-03-25 05:40:18 +0700

    Hapus Propellerads

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -58,8 +58,6 @@
     <link rel='dns-prefetch' href='//code.jquery.com' />
     <link rel='dns-prefetch' href='//stackpath.bootstrapcdn.com' />
 
-    <script src="/js/propellerads.js"></script>
-
     <!-- Scripts -->
     @routes
     @viteReactRefresh
