8dc9ab7c04662071f52eb5acff095534cfb30bd9
dadan nurmaulana <dadannurmaulana@gmail.com>
2023-03-29 05:39:55 +0700

    Edit pagination

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -4,31 +4,33 @@ import React from "react";
 const Pagination = ({ links }) => {
     return (
         <div>
-            {/* <div className="table-responsive"> */}
-            <nav aria-label="Page navigation example mt-4 ">
-                <ul className="pagination mt-3">
-                    {links.map((link, k) => (
-                        <div key={k}>
-                            <li
-                                className={`page-item p-1 ${
-                                    link.active && "active"
-                                } ${link.url === null ? "disabled" : ""}`}
-                            >
-                                <Link
-                                    disabled={link.url === null ? true : false}
-                                    as="button"
-                                    href={link.url}
-                                    className="page-link"
-                                    dangerouslySetInnerHTML={{
-                                        __html: link.label,
-                                    }}
-                                />
-                            </li>
-                        </div>
-                    ))}
-                </ul>
-            </nav>
-            {/* </div> */}
+            <div className="table-responsive">
+                <nav aria-label="Page navigation example mt-4 ">
+                    <ul className="pagination mt-3">
+                        {links.map((link, k) => (
+                            <div key={k}>
+                                <li
+                                    className={`page-item p-1 ${
+                                        link.active && "active"
+                                    } ${link.url === null ? "disabled" : ""}`}
+                                >
+                                    <Link
+                                        disabled={
+                                            link.url === null ? true : false
+                                        }
+                                        as="button"
+                                        href={link.url}
+                                        className="page-link"
+                                        dangerouslySetInnerHTML={{
+                                            __html: link.label,
+                                        }}
+                                    />
+                                </li>
+                            </div>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
