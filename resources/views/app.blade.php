97580be08d90b9d6976ba78589b14c17a25a19dd
Dadan28 <dadannurmaulana@gmail.com>
2023-04-04 11:49:19 +0700

    Hapus Tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,23 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <title inertia>{{ config('app.name', 'Laravel') }}</title> --}}
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-185896191-3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'UA-185896191-3');
-    </script>
-
-
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3755518164222853"
         crossorigin="anonymous"></script>
-
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -33,30 +18,13 @@
     {{-- <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@300&display=swap" rel="stylesheet"> --}}
 
     <!-- our project just needs Font Awesome Solid + Brands -->
-    <link href="/fontawesome/css/fontawesome.css" rel="stylesheet">
-    <link href="/fontawesome/css/brands.css" rel="stylesheet">
-    <link href="/fontawesome/css/solid.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/offcanvas.css">
 
-
-    <link rel='dns-prefetch' href='//www.lokersubang.com' />
-    <link rel='dns-prefetch' href='//lokersubang.com' />
-    <link rel='dns-prefetch' href='//fonts.gstatic.com' />
-    <link rel='dns-prefetch' href='//s.w.org' />
-    <link rel='dns-prefetch' href='//fonts.googleapis.com' />
-    <link rel='dns-prefetch' href='//ajax.googleapis.com' />
-    <link rel='dns-prefetch' href='//google-analytics.com' />
-    <link rel='dns-prefetch' href='//googletagmanager.com' />
-    <link rel='dns-prefetch' href='//cdnjs.cloudflare.com' />
-    <link rel='dns-prefetch' href='//googletagservices.com' />
-    <link rel='dns-prefetch' href='//use.fontawesome.com' />
-    <link rel='dns-prefetch' href='//www.google-analytics.com' />
-    <link rel='dns-prefetch' href='//ssl.google-analytics.com' />
-    <link rel='dns-prefetch' href='//www.googletagmanager.com' />
-    <link rel='dns-prefetch' href='//www.googletagservices.com' />
-    <link rel='dns-prefetch' href='//googleads.g.doubleclick.net' />
-    <link rel='dns-prefetch' href='//code.jquery.com' />
-    <link rel='dns-prefetch' href='//stackpath.bootstrapcdn.com' />
+    <link rel='dns-prefetch' href='https://www.lokersubang.com' />
+    <link rel='dns-prefetch' href='https://lokersubang.com' />
+    <link rel='dns-prefetch' href='https://fonts.gstatic.com' />
+    <link rel='dns-prefetch' href='https://s.w.org' />
+    <link rel='dns-prefetch' href='https://fonts.googleapis.com' />
 
     <!-- Scripts -->
     @routes
