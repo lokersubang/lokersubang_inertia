acf0ea8ae37c96f4c1fcabdfd0fa5f4c3da01f0e
Dadan28 <dadannurmaulana@gmail.com>
2023-04-08 04:29:40 +0700

    Edit Post

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -5,8 +5,8 @@ const Pagination = ({ links }) => {
     return (
         <div>
             <div className="table-responsive">
-                <nav aria-label="Page navigation example mt-4 ">
-                    <ul className="pagination mt-3">
+                <nav aria-label="Page navigation example mt-2 ">
+                    <ul className="pagination">
                         {links.map((link, k) => (
                             <div key={k}>
                                 <li
