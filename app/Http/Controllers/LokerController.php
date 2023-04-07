f5e90ca474ad86234a36158eb3ab37d3c48dc6b5
Dadan28 <dadannurmaulana@gmail.com>
2023-04-08 06:54:15 +0700

    Tambah Cache di Controller LokerController

--- app/Http/Controllers/LokerController.php
+++ app/Http/Controllers/LokerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class LokerController extends Controller
 {
@@ -17,14 +18,23 @@ class LokerController extends Controller
     public function index(Request $request)
     {
         $title = null;
+        $category = Cache::remember('category', 10 * 60, function () {
+            return Category::firstWhere('slug', request('kategori'));
+        });
         if (request('kategori')) {
-            $cat = Category::firstWhere('slug', request('kategori'));
+            $cat = $category;
+            // $cat = Category::firstWhere('slug', request('kategori'));
             $title = $cat->name;
         };
 
+
+        $lokers = Cache::remember('lokers', 10 * 60, function () {
+            return Post::latest()->filter(request(['search', 'kategori']))->with(['category', 'user'])->fastPaginate(10)->withQueryString();
+        });
         return inertia('Blog/Index', [
             'title' => $title,
-            'lokers' => Post::latest()->filter(request(['search', 'kategori']))->with(['category', 'user'])->fastPaginate(10)->withQueryString()
+            'lokers' => $lokers
+            // 'lokers' => Post::latest()->filter(request(['search', 'kategori']))->with(['category', 'user'])->fastPaginate(10)->withQueryString()
         ]);
     }
 
