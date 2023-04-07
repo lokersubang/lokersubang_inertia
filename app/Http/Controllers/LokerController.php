f03d9e87fc0257eae25ed7b70ceec3a7f4077a42
Dadan28 <dadannurmaulana@gmail.com>
2023-04-08 04:26:25 +0700

    Edit Post

--- app/Http/Controllers/LokerController.php
+++ app/Http/Controllers/LokerController.php
@@ -24,7 +24,7 @@ class LokerController extends Controller
 
         return inertia('Blog/Index', [
             'title' => $title,
-            'lokers' => Post::latest()->filter(request(['search', 'kategori']))->with(['category', 'user'])->fastPaginate(7)->withQueryString()
+            'lokers' => Post::latest()->filter(request(['search', 'kategori']))->with(['category', 'user'])->fastPaginate(10)->withQueryString()
         ]);
     }
 
